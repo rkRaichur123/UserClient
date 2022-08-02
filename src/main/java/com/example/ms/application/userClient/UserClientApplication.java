86134cd72d2f6fe82a2f4023ff931ba1b72a7209
rkRaichur123 <91364485+rkRaichur123@users.noreply.github.com>
2022-08-02 13:05:02 +0530

    User client

---
+++ src/main/java/com/example/ms/application/userClient/UserClientApplication.java
@@ -0,0 +1,25 @@
+package com.example.ms.application.userClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/health")
+public class UserClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserClientApplication.class, args);
+	}
+	
+	@GetMapping( value = "/healthCheck")
+	public String healthCheck() {
+		return ("HealthCheck is up and Running");
+	}
+
+}
