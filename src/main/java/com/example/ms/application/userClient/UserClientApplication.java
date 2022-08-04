e7af8ff0e0f2b19f950edb0a803f96c5a61fcde9
rkRaichur123 <91364485+rkRaichur123@users.noreply.github.com>
2022-08-04 16:41:39 +0530

    API gate way integration

--- src/main/java/com/example/ms/application/userClient/UserClientApplication.java
+++ src/main/java/com/example/ms/application/userClient/UserClientApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-@RequestMapping("/userClient")
+@RequestMapping("/api/user")
 public class UserClientApplication {
 
 	public static void main(String[] args) {
